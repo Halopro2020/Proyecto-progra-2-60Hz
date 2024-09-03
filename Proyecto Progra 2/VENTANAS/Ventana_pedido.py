1ba8799a8d9f470b48ec27d9ac9693b954a403e9
nose <109933877+Halopro2020@users.noreply.github.com>
2024-09-03 19:40:28 -0400

    Update Ventana_pedido.py

--- Proyecto Progra 2/VENTANAS/Ventana_pedido.py
+++ Proyecto Progra 2/VENTANAS/Ventana_pedido.py
@@ -5,11 +5,14 @@ import re
 
 def crear_panel_pedido(tab, ingresar_pedido_callback, eliminar_pedido_callback):
     
-    frame_treeview2 = ctk.CTkFrame(tab)
-    frame_treeview2.pack(side="bottom", fill="x", expand=True)
-        
     frame_inferior = ctk.CTkFrame(tab)
-    frame_inferior.pack(side="bottom",expand=False)
+    frame_inferior.pack(side="bottom", expand=False, anchor="center")
+
+    boton_generar_menu = ctk.CTkButton(frame_inferior, text="Generar Menu", command=ingresar_pedido_callback)
+    boton_generar_menu.pack(side="bottom", fill="x", expand=False)
+
+    frame_treeview2 = ctk.CTkFrame(tab)
+    frame_treeview2.pack(side="top", fill="both", expand=True)
 
     # Botón para eliminar menu arriba del Treeview
     boton_eliminar = ctk.CTkButton(frame_treeview2, text="Eliminar Menu", fg_color="black", text_color="white", command=eliminar_pedido_callback)
@@ -20,8 +23,4 @@ def crear_panel_pedido(tab, ingresar_pedido_callback, eliminar_pedido_callback):
     tree.heading("Nombre del menu", text="Nombre del menu")
     tree.heading("Cantidad", text="Cantidad")
     tree.heading("Precio Unitario", text="Precio Unitario")
-    tree.pack(expand=True, fill="x")
-
-    # Botón de menu
-    boton_generar_menu = ctk.CTkButton(frame_inferior, text="Generar Menu", command=ingresar_pedido_callback)
-    boton_generar_menu.pack(side="bottom", fill="x",expand=False)
+    tree.pack(expand=False, fill="both")
