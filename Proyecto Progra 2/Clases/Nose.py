b4823d6fb692df4c3cc0410440eeca51c9b2ff0a
nose <109933877+Halopro2020@users.noreply.github.com>
2024-09-03 12:13:57 -0400

    Update Nose.py

--- Proyecto Progra 2/Clases/Nose.py
+++ Proyecto Progra 2/Clases/Nose.py
@@ -1,32 +1,32 @@
-from Clases import Seno
+from Clases import Ingredientes
 
-class Biblioteca:
+class Guardar_ingrediente:
     def __init__(self):
-        self.lista_libros = []
+        self.lista_ingredientes = []
 
-    def agregar_ingrediente(self, libro):
-        # Buscar si el libro ya existe en la lista
-        for lib in self.lista_libros:
-            if lib.nombre == libro.nombre:
-                # Si el libro ya existe, actualizar la cantidad
-                lib.cantidad += libro.cantidad
+    def agregar_ingrediente(self, ingrediente):
+        # Buscar si el ingrediente ya existe en la lista
+        for lib in self.lista_ingredientes:
+            if lib.nombre == ingrediente.nombre:
+                # Si el ingrediente ya existe, actualizar la cantidad
+                lib.cantidad += ingrediente.cantidad
                 return True  # Cantidad actualizada
         
-        # Si el libro no existe, agregarlo a la lista
-        self.lista_libros.append(libro)
-        return True  # Libro agregado como nuevo
+        # Si el ingrediente no existe, agregarlo a la lista
+        self.lista_libros.append(ingrediente)
+        return True  # ingrediente agregado como nuevo
 
     def eliminar_ingrediente(self, Nombre,  Cantidad):
-        for lib in self.lista_libros:
+        for lib in self.lista_ingredientes:
             if lib.nombre == Nombre:
                 if lib.cantidad > int(Cantidad):
-                    # Reducir la cantidad en lugar de eliminar el libro
+                    # Reducir la cantidad en lugar de eliminar el ingrediente
                     lib.cantidad -= int(Cantidad)
                 else:
-                    # Si la cantidad es menor o igual, eliminar el libro
-                    self.lista_libros.remove(lib)
+                    # Si la cantidad es menor o igual, eliminar el ingrediente
+                    self.lista_ingredientes.remove(lib)
                 return True
         return False
 
     def obtener_ingredientes(self):
-        return [libro for libro in self.lista_libros]
+        return [libro for libro in self.lista_ingredientes]
