b253004ab8011c2f7e9713028cfd683fc5fb2436
brunoroja131 <bruno.rojas2024@alu.uct.cl>
2024-09-08 07:20:37 -0300

    ingredientes

--- Proyecto Progra 2/Clases/Ingredientes.py
+++ Proyecto Progra 2/Clases/Ingredientes.py
@@ -1,4 +1,8 @@
 class Ingrediente:
-    def __init__(self, nombre, cantidad):
+    def __init__(self, nombre, papas,  papas_fritas, completo, cola, hamburguesa, cantidad):
         self.nombre = nombre
+        self.papas_fritas = papas_fritas
+        self.completo = completo
+        self.cola = cola
+        self.hamburguesa = hamburguesa
         self.cantidad = cantidad
