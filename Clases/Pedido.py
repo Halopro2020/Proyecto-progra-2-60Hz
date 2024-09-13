a217e2b6119a0c80d295a4fa39f525b09178ca27
nose <109933877+Halopro2020@users.noreply.github.com>
2024-09-13 02:50:34 -0300

    Update Pedido.py

--- Clases/Pedido.py
+++ Clases/Pedido.py
@@ -1,11 +1,18 @@
+# Pedido.py
+from Clases.Menu import Menu  # Asegúrate de que esta ruta sea correcta según la estructura de tu proyecto
+
 class Pedido:
     def __init__(self):
-        self.menus = []  
+        self.menus = []
         self.total = 0
 
     def agregar_menu(self, menu):
-        self.menus.append(menu)
-        self.total += menu.precio
+        # Asegúrate de que 'menu' sea un objeto que tenga un atributo 'precio'
+        if isinstance(menu, Menu):
+            self.menus.append(menu)
+            self.total += menu.precio
+        else:
+            print("Error: El menú no es una instancia de la clase Menu.")
 
     def eliminar_menu(self, menu):
         if menu in self.menus:
