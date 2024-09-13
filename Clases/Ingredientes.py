0c1fe79bdf911ab43008862e7b431aee06e9f3bc
nose <109933877+Halopro2020@users.noreply.github.com>
2024-09-13 02:20:27 -0300

    Update Ingredientes.py

--- Clases/Ingredientes.py
+++ Clases/Ingredientes.py
@@ -1,13 +1,7 @@
 class Ingrediente:
-    def __init__(self, nombre, papas=0, bebida=0, hamburguesa=0, vienesa=0, cantidad=0, pan_completo=0, palta=0, tomate=0, lamina_queso=0, churrasco=0):
+    def __init__(self, nombre, cantidad):
         self.nombre = nombre
-        self.papas = papas
-        self.bebida = bebida
-        self.hamburguesa = hamburguesa
-        self.vienesa = vienesa
         self.cantidad = cantidad
-        self.pan_completo = pan_completo
-        self.palta = palta
-        self.tomate = tomate
-        self.lamina_queso = lamina_queso
-        self.churrasco = churrasco
+
+    def __repr__(self):
+        return f"Ingrediente(nombre={self.nombre}, cantidad={self.cantidad})"
